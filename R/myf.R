a61a7cb42eec7a6e092b6515021b026df4c67009
Lindsey DeLuga <lindseydeluga@ou.edu>
2020-05-18 16:42:49 -0500

    Initial file list

---
+++ R/myf.R
@@ -0,0 +1,14 @@
+#' Square function
+#'
+#' Takes a vector and returns a cector of squared components
+#'
+#' @param x
+#'
+#' @return a vector of squared components
+#' @export
+#'
+#' @examples
+#' x=1:30; myf(x)
+myf=function(x){
+  x^2
+}
